c3f42013f32238fa14fe2f2a7e2682d02bf73348
xdzurman <kamil.dzurman@gmail.com>
2019-11-23 20:55:11 +0100

    Add Resnet embedding layer to model

--- src/model/model.py
+++ src/model/model.py
@@ -1,7 +1,7 @@
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, UpSampling2D, InputLayer, BatchNormalization, concatenate
+import src.consts as c
 
-
-def model_layers(input):
+def model_layers(input, embed_input):
     # Going Down
     x = Conv2D(16, 3, padding='same', activation='relu')(input)
     x = down_1 = MaxPooling2D((2, 2), strides=(2, 2))(x)
@@ -16,7 +16,13 @@ def model_layers(input):
     x = down_4 = MaxPooling2D((2, 2), strides=(2, 2))(x)
 
     x = Conv2D(256, 3, padding='same', activation='relu')(x)
-
+    
+    # Embedding
+    f = RepeatVector(32 * 32)(embed_input) 
+    f = Reshape(([32, 32, c.EMBED_SIZE]))(f)
+    x = concatenate([x, f], axis=3) 
+    x = Conv2D(256, (1, 1), activation='relu', padding='same')(x) 
+    
     # Going Up
     x = concatenate([x, down_4])
     x = UpSampling2D((2, 2))(x)
--- src/model/train_model.py
+++ src/model/train_model.py
@@ -9,8 +9,9 @@ from src.helpers.dataset_helpers import create_tf_dataset, get_train_valid_test
 
 
 def create_model():
+    embed_input = Input(shape=(c.EMBED_SIZE,))
     input = Input(shape=(c.IMG_HEIGHT, c.IMG_WIDTH, 1), dtype=tf.dtypes.float32)
-    colorizing_model = Model(inputs=[input], outputs=[model.model_layers(input)])
+    colorizing_model = Model(inputs=[input, embed_input], outputs=[model.model_layers(input, embed_input)])
     colorizing_model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
 
     return colorizing_model
@@ -21,7 +22,7 @@ def train_model():
     batch_size = 16
 
     print(f'train images size {len(train_paths)}, valid images size {len(valid_paths)}, batch_size {batch_size}')
-    train_data = create_tf_dataset(train_paths, batch_size)
+    train_data = create_tf_dataset(train_paths[:10], batch_size)
     valid_data = create_tf_dataset(valid_paths, batch_size)
 
     model = create_model()
