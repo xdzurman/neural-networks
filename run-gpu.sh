46ad0a3c1f3aa4d7ad32baed0dc0ec6f23dca6e0
Nikolas Tomaštík <nikolas.tsk@gmail.com>
2019-11-30 17:47:21 +0100

    fixed run scripts

--- run-gpu.sh
+++ run-gpu.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --gpus all -p 8888:8888 -p 6006:6006 -v $(pwd)/:/project -it yimejky/nn-project
+docker run --gpus all -u $(id -u):$(id -g) -p 8888:8888 -p 6006:6006 -v $(pwd)/:/project -it yimejky/nn-project
